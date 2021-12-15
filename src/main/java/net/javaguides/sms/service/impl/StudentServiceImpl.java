10f7b91a685b2ef2516a179272bc8f8d0cc673d2
Deepshikha Das <deepshikha284@gmail.com>
2021-12-15 21:22:34 +0530

    Student Management System Update button

--- src/main/java/net/javaguides/sms/service/impl/StudentServiceImpl.java
+++ src/main/java/net/javaguides/sms/service/impl/StudentServiceImpl.java
@@ -3,6 +3,7 @@ package net.javaguides.sms.service.impl;
 import net.javaguides.sms.entity.Student;
 import net.javaguides.sms.repository.StudentRepository;
 import net.javaguides.sms.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 @Service
 public class StudentServiceImpl implements StudentService {
 
+    @Autowired
     private StudentRepository studentRepository;
 
     public StudentServiceImpl(StudentRepository studentRepository) {
